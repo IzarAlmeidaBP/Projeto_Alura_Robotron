c01722430ef1d3b88b6c0700a90b4da288474444
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-05-18 16:09:55 -0300

    feat: adicionando ou retirando peças

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const adicionar = document.querySelector("#adicionar");
+const remover = document.querySelector("#remover");
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.textContent, evento.target.parentNode);
+  });
+});
+
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector(".controle-contador");
+  if (operacao == "-") {
+    peca.value = parseInt(peca.value) - 1;
+  } else {
+    peca.value = parseInt(peca.value) + 1;
+  }
+}
