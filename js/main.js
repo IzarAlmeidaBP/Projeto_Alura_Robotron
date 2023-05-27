7d82fe0424c6f156dd8d4ba2c325d87dd003458c
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-05-27 15:46:37 -0300

    feat: corrigindo as estatisticas

--- js/main.js
+++ js/main.js
@@ -1,38 +1,46 @@
 const controle = document.querySelectorAll("[data-controle]");
 const estatisticas = document.querySelectorAll("[data-estatistica");
+
+
+const maxForca = 41;
+const maxBlindagem = 41;
+const maxNucleos = 48;
+const maxPernas = 42;
+const maxFoguetes = 28;
+
 const pecas = {
   bracos: {
-    forca: 29,
+    forca: Math.round((29 / maxForca) * 100),
     poder: 35,
     energia: -21,
     velocidade: -5,
   },
-
   blindagem: {
-    forca: 41,
+    forca: Math.round((41 / maxBlindagem) * 100),
     poder: 20,
     energia: 0,
     velocidade: -20,
   },
   nucleos: {
-    forca: 0,
+    forca: Math.round((48 / maxNucleos) * 100),
     poder: 7,
     energia: 48,
     velocidade: -24,
   },
   pernas: {
-    forca: 27,
+    forca: Math.round((27 / maxPernas) * 100),
     poder: 21,
     energia: -32,
     velocidade: 42,
   },
   foguetes: {
-    forca: 0,
+    forca: Math.round((0 / maxFoguetes) * 100),
     poder: 28,
     energia: 0,
     velocidade: -2,
   },
 };
+
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
